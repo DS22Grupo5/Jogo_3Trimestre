a2e8028fb1b52b1976a6ba32381d178c95c4e13f
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2022-11-29 14:12:10 -0300

    removi script não utilizado

--- LP _DanielMosca/TerceiraFaseFinal/Assets/Scripts/Spawner.cs
+++ LP _DanielMosca/TerceiraFaseFinal/Assets/Scripts/Spawner.cs
@@ -8,13 +8,14 @@ public class Spawner : MonoBehaviour
     public float spawnRate;
 
     private float nextSpawn = 0f;
-
+    public float nitaners = 0;
 
 
     // Update is called once per frame
     void Update()
     {
         if (Time.time > nextSpawn){
+            nitaners++;
                 nextSpawn = Time.time + spawnRate;
                 Instantiate(enemy,transform.position,enemy.transform.rotation);
         }
